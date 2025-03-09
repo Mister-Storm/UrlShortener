57883e789dc24e3a12f7c65dcd49d752147715cb
mister-storm <clfernando1979@hotmail.com>
2025-03-09 14:27:14 -0300

    improve ci with it

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.25"
     id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.7"
+    id("jacoco")
 }
 
 group = "br.com.misterstorm"
@@ -42,4 +43,49 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+
+sourceSets {
+    val intTest by creating {
+        compileClasspath += sourceSets["main"].output + sourceSets["test"].output
+        runtimeClasspath += sourceSets["main"].output + sourceSets["test"].output
+    }
+}
+
+configurations {
+    val intTestImplementation by getting {
+        extendsFrom(configurations["testImplementation"])
+    }
+    val intTestRuntimeOnly by getting {
+        extendsFrom(configurations["testRuntimeOnly"])
+    }
+}
+
+tasks.register<Test>("integrationTest") {
+    description = "Runs integration tests."
+    group = "verification"
+    testClassesDirs = sourceSets["intTest"].output.classesDirs
+    classpath = sourceSets["intTest"].runtimeClasspath
+    shouldRunAfter(tasks.test)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.named<Test>("test"), tasks.named<Test>("integrationTest"))
+
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+
+    executionData.setFrom(
+        executionData.setFrom(
+            fileTree(layout.buildDirectory).include(
+                "jacoco/test.exec",
+                "jacoco/integrationTest.exec"
+            )
+        )
+    )
+
+    sourceSets(sourceSets["main"])
 }
